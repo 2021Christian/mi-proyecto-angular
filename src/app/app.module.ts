ed095ee1a886d7511d567e2ce367d1d85c5fb00b
Christian Ghione <cghione@gmail.com>
2022-03-15 23:33:24 -0300

    ComponentesBase Binding-Estructura-Atributos-Formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BindingComponent } from './componentes/binding/binding.component';
+import { EstructuraComponent } from './componentes/estructura/estructura.component';
+import { AtributosComponent } from './componentes/atributos/atributos.component';
+import { FormulariosComponent } from './componentes/formularios/formularios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BindingComponent,
+    EstructuraComponent,
+    AtributosComponent,
+    FormulariosComponent
   ],
   imports: [
     BrowserModule
