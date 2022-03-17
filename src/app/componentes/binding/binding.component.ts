45a71468bf73e26dff36d1067a48b1f3667b26d0
Christian Ghione <cghione@gmail.com>
2022-03-17 12:05:08 -0300

    Componente Binding

--- src/app/componentes/binding/binding.component.ts
+++ src/app/componentes/binding/binding.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BindingComponent implements OnInit {
 
+  mensaje:string = "Soy mensaje";
+  valor1:number = 123;
+  valor2:number = 456;
+  contador:number = 0;
+  valor3:number = 789;
+  valor4:number = 321;
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  modificar(e:any) {
+    // console.log(e);
+    let valor = e.target.value;
+    // console.log(valor);
+    this.valor3 = valor;
+
   }
 
 }
