7b2cfc596e32092a2518f3ad1dbfd1f8dc67a765
Christian Ghione <cghione@gmail.com>
2022-03-18 00:00:25 -0300

    Componente Atributos

--- src/app/componentes/atributos/atributos.component.ts
+++ src/app/componentes/atributos/atributos.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AtributosComponent implements OnInit {
 
+  estado1:boolean = true;
+  estado2:boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cambiarPropiedad(estado:boolean) {
+    return {
+      backgroundColor: estado?'blue':'crimson',
+      color: 'white',
+      padding: '10px',
+      borderRadius: '10px'}
+  }
+
+  cambiarClase(estado:boolean) {
+    return {
+      btn : true,
+      'btn-primary': estado,
+      'btn-danger': !estado
+    }
   }
 
 }
