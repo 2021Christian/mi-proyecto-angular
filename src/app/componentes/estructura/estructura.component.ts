8befea6e45cf6c811b22e3186ce6261d613ea119
Christian Ghione <cghione@gmail.com>
2022-03-17 23:29:56 -0300

    Componente Estructura

--- src/app/componentes/estructura/estructura.component.ts
+++ src/app/componentes/estructura/estructura.component.ts
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstructuraComponent implements OnInit {
 
+  mostrar:boolean = true;
+
+  // personas:string[] = [
+  personas:Array<string> = [
+    "Pedro",
+    "Cecilia",
+    "Maria",
+    "Tomás"
+  ];
+
+  // alumnos:any[] = [
+  alumnos:Array<any> = [
+    {nombre: 'Juan',    apellido: 'Perez',    edad: 23, curso: false,   foto: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/2_avatar-256.png'},
+    {nombre: 'Carlos',  apellido: 'Gomez',    edad: 34, curso: true,    foto: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/7_avatar-256.png'},
+    {nombre: 'Augusto', apellido: 'Suarez',   edad: 37, curso: false,   foto: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/1_avatar-256.png'},
+    {nombre: 'Lucía',   apellido: 'Blanco',   edad: 31, curso: true,    foto: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/5_avatar-256.png'}
+  ];
+
+  // https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/4_avatar-256.png
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  borrarAlumno(index:number) {
+    this.alumnos.splice(index,1);
+  }
+
+  agregarAlumno() {
+    let alumno = {
+      nombre: 'Maria',
+      apellido: 'Lopez',
+      edad: 35,
+      curso: false,
+      foto: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/4_avatar-256.png'
+    }
+
+    this.alumnos.push(alumno);
+
+  }
+
 
 }
